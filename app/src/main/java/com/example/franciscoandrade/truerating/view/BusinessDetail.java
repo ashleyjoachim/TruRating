58936edf986a84025b152ab0826d521613087abf
Melina Gonzalez <melg@Melinas-MacBook-Pro.local>
2018-03-03 17:31:04 -0500

    detail

---
+++ app/src/main/java/com/example/franciscoandrade/truerating/view/BusinessDetail.java
@@ -0,0 +1,20 @@
+package com.example.franciscoandrade.truerating.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.franciscoandrade.truerating.R;
+
+/**
+ * Created by melg on 3/3/18.
+ */
+
+public class BusinessDetail extends AppCompatActivity{
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.detail_layout);
+    }
+
+}
