7cdecb5d9f2b21fc52b647ce6cbd1446cbffa508
Melina Gonzalez <melg@Melinas-MacBook-Pro.local>
2018-03-03 12:38:21 -0500

    map options

--- app/src/main/java/com/example/franciscoandrade/truerating/controller/GradingAdapter.java
+++ app/src/main/java/com/example/franciscoandrade/truerating/controller/GradingAdapter.java
@@ -1,7 +1,11 @@
 package com.example.franciscoandrade.truerating.controller;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.franciscoandrade.truerating.R;
 
 /**
  * Created by franciscoandrade on 3/3/18.
@@ -11,17 +15,17 @@ public class GradingAdapter extends RecyclerView.Adapter<GradingViewHolder> {
 
 
 
-    public GradingAdapter(){
 
-    }
 
     @Override
     public GradingViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+       return null;
     }
 
     @Override
     public void onBindViewHolder(GradingViewHolder holder, int position) {
+
+
 
     }
 
--- app/src/main/java/com/example/franciscoandrade/truerating/view/MainActivity.java
+++ app/src/main/java/com/example/franciscoandrade/truerating/view/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.franciscoandrade.truerating.view;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.franciscoandrade.truerating.R;
 import com.example.franciscoandrade.truerating.backend.RestApi;
@@ -51,6 +56,22 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.options_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+
+        Intent intent = new Intent(getApplicationContext(), MapActivity.class);
+        startActivity(intent);
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
