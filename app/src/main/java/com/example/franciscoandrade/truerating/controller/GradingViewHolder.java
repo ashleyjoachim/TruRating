19361151c80754bded28e1676b4d543df9df2884
Melina Gonzalez <melg@Melinas-MacBook-Pro.local>
2018-03-03 12:06:01 -0500

    Recycler view

---
+++ app/src/main/java/com/example/franciscoandrade/truerating/controller/GradingViewHolder.java
@@ -0,0 +1,14 @@
+package com.example.franciscoandrade.truerating.controller;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by melg on 3/3/18.
+ */
+
+public class GradingViewHolder extends RecyclerView.ViewHolder {
+    public GradingViewHolder(View itemView) {
+        super(itemView);
+    }
+}
