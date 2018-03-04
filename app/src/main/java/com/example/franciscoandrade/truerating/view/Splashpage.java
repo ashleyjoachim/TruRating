3cdd7eaa14f4d5cbd501cdd51a45782693795a8e
Melina Gonzalez <melg@Melinas-MacBook-Pro.local>
2018-03-04 12:06:27 -0500

    logo splash page

--- app/src/main/java/com/example/franciscoandrade/truerating/view/Splashpage.java
+++ app/src/main/java/com/example/franciscoandrade/truerating/view/Splashpage.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -23,24 +24,15 @@ import com.example.franciscoandrade.truerating.R;
 public class Splashpage extends AppCompatActivity {
     private VideoView videoView;
     private String TAG;
-    private Button button;
+    private static int SPLASH_TIME_OUT = 3000;
 
-    @SuppressLint("ResourceAsColor")
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_page);
 
         videoView = findViewById(R.id.video_view);
-        button = findViewById(R.id.next_page_button);
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Splashpage.this,MapsActivity.class);
-                startActivity(intent);
-            }
-        });
 
         String uriPath = "android.resource://" + getPackageName() + "/"
                 + R.raw.trurating_animatedd;
@@ -52,9 +44,31 @@ public class Splashpage extends AppCompatActivity {
         videoView.setZOrderOnTop(true);
         videoView.start();
 
+
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(Splashpage.this, MapsActivity.class);
+                startActivity(i);
+
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
     }
 
 
-
 }
+
+
+
+
 
